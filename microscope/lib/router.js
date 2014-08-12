56924d4696cac2923fe656ba53135b1227c946ae
Alexander Pan <alexanderdavidpan@gmail.com>
2014-08-12 15:54:48 +0800

    Pass data variable to single post page - Returns post based on post id

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -10,7 +10,8 @@ Router.map(function() {
     });
 
     this.route('postPage', { 
-        path: '/posts/:_id'
+        path: '/posts/:_id',
+        data: function() { return Posts.findOne(this.params._id); }
     });
 });
 
