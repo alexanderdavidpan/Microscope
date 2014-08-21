af786b7066ee05db14e080439907987e10876631
Alexander Pan <alexanderdavidpan@gmail.com>
2014-08-21 10:01:28 -0700

    Insert error message seen as false - When a new error message is inserted into the client-side collection, it is marked as seen false - This is because although it is in the collection, the user has not seen it yet - Error will become seen true when the error template is rendered

--- microscope/client/helpers/errors.js
+++ microscope/client/helpers/errors.js
@@ -2,7 +2,7 @@
 Errors = new Meteor.Collection(null);
 
 throwError = function(message) { 
-    Errors.insert({message: message})
+    Errors.insert({message: message, seen: false})
 }
 
 clearErrors = function() { 
