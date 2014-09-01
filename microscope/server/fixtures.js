ff3f69590cfda8137c89a37c2be5e9a31583d80d
Alexander Pan <alexanderdavidpan@gmail.com>
2014-09-01 00:15:07 -0700

    Add a lot of posts to fixtures file - Add a lot of posts into the Posts collection in the fixtures file to make pagination necessary

--- microscope/server/fixtures.js
+++ microscope/server/fixtures.js
@@ -5,11 +5,11 @@ if (Posts.find().count() === 0) {
   // create two users
   var tomId = Meteor.users.insert({
     profile: { name: 'Tom Coleman' }
-});
+  });
   var tom = Meteor.users.findOne(tomId);
   var sachaId = Meteor.users.insert({
     profile: { name: 'Sacha Greif' }
-});
+  });
   var sacha = Meteor.users.findOne(sachaId);
   
   var telescopeId = Posts.insert({
@@ -19,7 +19,7 @@ if (Posts.find().count() === 0) {
     url: 'http://sachagreif.com/introducing-telescope/',
     submitted: now - 7 * 3600 * 1000,
     commentsCount: 2
-});
+  });
   
   Comments.insert({
     postId: telescopeId,
@@ -27,7 +27,7 @@ if (Posts.find().count() === 0) {
     author: tom.profile.name,
     submitted: now - 5 * 3600 * 1000,
     body: 'Interesting project Sacha, can I get involved?'
-});
+  });
   
   Comments.insert({
     postId: telescopeId,
@@ -35,7 +35,7 @@ if (Posts.find().count() === 0) {
     author: sacha.profile.name,
     submitted: now - 3 * 3600 * 1000,
     body: 'You sure can Tom!'
-});
+  });
   
   Posts.insert({
     title: 'Meteor',
@@ -44,7 +44,7 @@ if (Posts.find().count() === 0) {
     url: 'http://meteor.com',
     submitted: now - 10 * 3600 * 1000,
     commentsCount: 0
-});
+  });
   
   Posts.insert({
     title: 'The Meteor Book',
@@ -53,5 +53,16 @@ if (Posts.find().count() === 0) {
     url: 'http://themeteorbook.com',
     submitted: now - 12 * 3600 * 1000,
     commentsCount: 0
-});
+  });
+  
+  for (var i = 0; i < 10; i++) {
+    Posts.insert({
+      title: 'Test post #' + i,
+      author: sacha.profile.name,
+      userId: sacha._id,
+      url: 'http://google.com/?q=test-' + i,
+      submitted: now - i * 3600 * 1000 + 1,
+      commentsCount: 0
+    });
+  }
 }
