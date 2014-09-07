140fb55b45a0501c90eca22c0862f7526ef1defb
Alexander Pan <alexanderdavidpan@gmail.com>
2014-09-07 12:48:07 -0700

    Add singlePost subscription to postEdit route - waitOn function will load singlePost info as well as comments related to the post - singlePost subscription finds the Post by using the _id

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -45,6 +45,9 @@ Router.map(function() {
 
   this.route('postEdit', {
     path: '/posts/:_id/edit',
+    waitOn: function() { 
+        return Meteor.subscribe('singlePost', this.params._id);
+    },
     data: function() { return Posts.findOne(this.params._id); }
   });
   
